3b7a6232837e888c3bddf5b9a783ed6462d0dfe9
Vatsala <vats15shekhar@gmail.com>
2024-04-14 18:32:07 +0530

    Starting Java Concepts Project

---
+++ src/main/java/com/javaConcepts/JavaConcepts/JavaConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.javaConcepts.JavaConcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaConcepts/JavaConcepts/JavaConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaConcepts.JavaConcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
